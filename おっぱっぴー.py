1b10955c827e5c26e5835e7a73c4f2673e34dac9
hisiragi/ひしらぎ <127497658+hisiragi@users.noreply.github.com>
2026-07-02 23:19:14 +0900

    大島よしおさん

---
+++ おっぱっぴー.py
@@ -0,0 +1,9 @@
+import random
+
+print("でもそんなの関係ねぇ")
+print("でもそんなの関係ねぇ")
+
+if random.random() < 0.5:
+    print("でもそんなの関係ねぇ")
+
+print("はい、おっぱっぴー")
---
+++ 間違い探し.py
@@ -0,0 +1,9 @@
+import random
+
+print("でもそんなの関係ねぇ")
+print("でもそんなの関係ねぇ")
+
+if random.randint(0, 1) == 1:
+    print("でもそんなの関係ねぇ")
+
+print("はい、おっぱっぴー")
